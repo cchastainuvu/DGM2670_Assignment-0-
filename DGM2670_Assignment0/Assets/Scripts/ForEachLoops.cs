45b5c2859f304111eada9f33187b9313e70e462d
Unknown <10798368@my.uvu.edu>
2018-09-19 23:27:16 -0600

    scripts transferred manually

--- DGM2670_Assignment0/Assets/Scripts/ForEachLoops.cs
+++ DGM2670_Assignment0/Assets/Scripts/ForEachLoops.cs
@@ -4,33 +4,33 @@ using System.Runtime.Remoting.Messaging;
 using UnityEditor;
 using UnityEngine;
 
-[CreateAssetMenu]
+//[CreateAssetMenu]
 public class ForEachLoops : ScriptableObject
 {
-
-public FloatData RandomSpeed;
-public List< <FloatData> SpeedOptions;
-
-
-    private void OnEnable()
-    {
-        var randomNum = Random.Range(0, SpeedOptions.Count - 1);
-        //0 , Size of the Collection, so however many have been set.
-        //Minus 1, because Unity starts counting at zero.
-
-        for (int i = 0; i < SpeedOptions.count-1; i++)
-        {
-            //Checks to see if i is zero, then checks to see if i is less than count, then add to i.
-            // i loops through every single number and compares, then takes the speed and sets it.
-
-            if (i == randomNum)
-            {
-                RandomSpeed == SpeedOptions[i];
-            }
-            
-        }
-    
-    }
+//
+//public FloatData RandomSpeed;
+//public List< <FloatData> SpeedOptions;
+//
+//
+//    private void OnEnable()
+//    {
+//        var randomNum = Random.Range(0, SpeedOptions.Count - 1);
+//        //0 , Size of the Collection, so however many have been set.
+//        //Minus 1, because Unity starts counting at zero.
+//
+//        for (int i = 0; i < SpeedOptions.count-1; i++)
+//        {
+//            //Checks to see if i is zero, then checks to see if i is less than count, then add to i.
+//            // i loops through every single number and compares, then takes the speed and sets it.
+//
+//            if (i == randomNum)
+//            {
+//                RandomSpeed == SpeedOptions[i];
+//            }
+//            
+//        }
+//    
+//    }
 }
 
 //This is kind of what I did with the respawn location for the wolves and the ChickenHat.
