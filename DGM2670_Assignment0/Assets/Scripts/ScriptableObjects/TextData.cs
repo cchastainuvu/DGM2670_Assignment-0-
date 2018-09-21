d8a8b9b16b1a11aaf03240dce4104588b53e8910
Unknown <10798368@my.uvu.edu>
2018-09-20 22:35:18 -0600

    TextData ScriptableObject

---
+++ DGM2670_Assignment0/Assets/Scripts/ScriptableObjects/TextData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu]
+public class TextData : ScriptableObject
+{ 
+    public Text text;
+
+    public virtual Text Text
+    {
+        get { return text; }
+    }
+
+}
